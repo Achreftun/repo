565299eef81075e465017c0aaa0fc53b806b247f
achref <elmouelhi.achref@gmail.com>
2021-06-18 07:41:15 +0200

    création de projet

---
+++ cours-java/src/org/eclipse/record/Point.java
@@ -0,0 +1,5 @@
+package org.eclipse.record;
+
+public record Point(int x, int y) {
+
+}
---
+++ cours-java/src/org/eclipse/test/Main.java
@@ -0,0 +1,31 @@
+package org.eclipse.test;
+
+import org.eclipse.record.Point;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		Point p1 = new Point(10, 20);
+		System.out.println(p1.x()); // 10
+		System.out.println(p1.y()); // 20
+
+		Point p2 = new Point(11, 22);
+		System.out.println(p2.x()); // 11
+		System.out.println(p2.y()); // 22
+
+		Point p3 = new Point(10, 20);
+		System.out.println(p3.x()); // 10
+		System.out.println(p3.y()); // 20
+
+		System.out.println(p1.hashCode()); // 330
+		System.out.println(p2.hashCode()); // 363
+		System.out.println(p3.hashCode()); // 330
+
+		System.out.println(p1.equals(p2)); // false
+		System.out.println(p1.equals(p3)); // true
+		System.out.println(p2.equals(p3)); // false
+	}
+
+}
